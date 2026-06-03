ea717ede4641495eeda8322b62e32a013b475b85
Ayansh <ayanshsaroj94@gmail.com>
2026-06-03 17:52:21 +0530

    updated the app.js for register API routes for users, habits, posts, and leaderboards

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import {ApiResponse} from "./utils/ApiResponse.js"
+import userRouter from "./routes/user.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+
+import habitRouter from "./routes/habit.routes.js";
+
+import postRouter from "./routes/post.routes.js";
+
+import leaderboardRouter from "./routes/leaderboard.routes.js";
 
 
 const app = express();
@@ -15,10 +23,20 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-app.get("/api/health",(req,res)=>{
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/habits", habitRouter);
+
+app.use("/api/v1/posts", postRouter);
+
+app.use("/api/v1/leaderboards", leaderboardRouter);
+
+app.get("/api/v1/health",(req,res)=>{
     return res.status(200).json(
         new ApiResponse(200, null, "Server is healthy and running")
     )
-})
+});
+
+app.use(errorHandler);
 
 export { app };
