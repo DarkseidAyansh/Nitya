5b996e4d960eb97aed86d8046616e5ff42696858
Ayansh <ayanshsaroj94@gmail.com>
2026-06-03 17:43:12 +0530

    added routes for getcomments

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -4,16 +4,17 @@ import {
     createPost, 
     getFeed, 
     toggleReaction, 
-    addComment 
+    addComment, 
+    getComments
 } from "../controllers/post.controller.js";
 
 const router = Router();
 
-// All routes require authentication
 router.use(verifyJWT);
 
 router.route("/").post(createPost).get(getFeed);
 router.route("/:postId/reaction").post(toggleReaction);
 router.route("/:postId/comments").post(addComment);
+router.route("/:postId/comments").get(getComments);
 
 export default router;
