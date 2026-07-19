7795b1a3e041def6dc39525bd18a5fd9b5586ea9
Ayansh <ayanshsaroj94@gmail.com>
2026-07-19 22:24:00 +0530

    add Constants file for global configuration

---
+++ android/app/src/main/java/com/example/nityaandroid/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.nityaandroid.utils
+
+object Constants {
+    const val BASE_URL = "https://habit-tracker-api-pfq0.onrender.com/api/v1/"
+    const val DATABASE_NAME = "habit_streak_db"
+}
